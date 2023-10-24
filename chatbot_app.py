e0f6fd25dd61dcc2a63f3938862d16357f7ccedf
Guptabhimanyu <96366301+Guptabhimanyu@users.noreply.github.com>
2023-10-24 22:16:42 +0530

    Update chatbot_app.py

--- chatbot_app.py
+++ chatbot_app.py
@@ -15,6 +15,14 @@ from langchain.chains import RetrievalQA
 from constants import CHROMA_SETTINGS
 from streamlit_chat import message
 
+# Replace the old import statement
+from pydantic import BaseSettings
+
+# With the new import statement
+from pydantic_settings import BaseSettings
+
+
+
 import torch
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
