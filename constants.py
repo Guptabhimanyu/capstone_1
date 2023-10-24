59cf819f00dc309f6d7f925bee2b17547e4c3aaf
Guptabhimanyu <96366301+Guptabhimanyu@users.noreply.github.com>
2023-10-24 22:30:34 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,3 @@
-# Replace the old import statement
-from pydantic import BaseSettings
-
-# With the new import statement
-from pydantic_settings import BaseSettings
 import os 
 import chromadb
 from chromadb.config import Settings 
